0dffbffe82142eb29477643d2010af21b2f57116
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-02 14:01:20 -0300

    :wrench: Configure hl-group gitcommitOverflow

--- lua/nano-theme/groups/integrations/neogit.lua
+++ lua/nano-theme/groups/integrations/neogit.lua
@@ -10,6 +10,8 @@ function M.get()
   local c = require("nano-theme.colors").get()
 
   return {
+    gitcommitOverflow         = { link = "Error" },
+
     NeogitCommitViewHeader    = { fg = c.fg, bg = c.nano_subtle_color, bold = true },
     NeogitDiffAdd             = { fg = c.nano_salient_color, bold = true },
     NeogitDiffAddCursor       = { fg = c.nano_salient_color, bg = c.nano_highlight_color, bold = true },
