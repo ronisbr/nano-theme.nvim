44ad531548bab0be76a7bf65aa100d5f1d249eba
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-02 13:50:02 -0300

    :wrench: Add missing hl-groups in Neogit

--- lua/nano-theme/groups/integrations/neogit.lua
+++ lua/nano-theme/groups/integrations/neogit.lua
@@ -12,8 +12,10 @@ function M.get()
   return {
     NeogitCommitViewHeader    = { fg = c.fg, bg = c.nano_subtle_color, bold = true },
     NeogitDiffAdd             = { fg = c.nano_salient_color, bold = true },
+    NeogitDiffAddCursor       = { fg = c.nano_salient_color, bg = c.nano_highlight_color, bold = true },
     NeogitDiffAddHighlight    = { link = "NeogitDiffAdd" },
     NeogitDiffDelete          = { fg = c.nano_popout_color, bold = true },
+    NeogitDiffDeleteCursor    = { fg = c.nano_popout_color, bg = c.nano_highlight_color, bold = true },
     NeogitDiffDeleteHighlight = { link = "NeogitDiffDelete" },
     NeogitHunkHeader          = { fg = c.fg, bg = c.nano_highlight_color },
     NeogitHunkHeaderHighlight = { fg = c.fg, bg = c.nano_subtle_color, bold = true },
