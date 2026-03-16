3159e98aa9d31383d6ef44c09d8b24d1f40d254c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-03-15 21:39:13 -0300

    :sparkles: Add Rougier's variant

--- lua/nano-theme/colors.lua
+++ lua/nano-theme/colors.lua
@@ -10,7 +10,10 @@ local M = {}
 --                                   Private Functions                                    --
 --------------------------------------------------------------------------------------------
 
--- Build the full color table from the raw color values and terminal color mapping.
+--- Build the full color table from the raw color values and terminal color mapping.
+---@param raw table Table of raw hex color strings and terminal color values.
+---@return table Color table with named semantic colors, highlight decorations, and terminal
+---              colors.
 local function build_colors(raw)
   return {
     -- Base Ramp ---------------------------------------------------------------------------
@@ -77,6 +80,8 @@ end
 
 -- Default Variant -------------------------------------------------------------------------
 
+--- Return the color table for the default light variant.
+---@return table Color table for the default light variant.
 local function default_light()
   local base6   = "#484840"
   local red     = "#C03830"
@@ -124,6 +129,8 @@ local function default_light()
   })
 end
 
+--- Return the color table for the default dark variant.
+---@return table Color table for the default dark variant.
 local function default_dark()
   local base6   = "#908E88"
   local red     = "#E84040"
@@ -173,6 +180,8 @@ end
 
 -- Black-White Variant ---------------------------------------------------------------------
 
+--- Return the color table for the black-white dark variant.
+---@return table Color table for the black-white dark variant.
 local function black_white_dark()
   local base6   = "#707070"
   local red     = "#E84040"
@@ -220,6 +229,8 @@ local function black_white_dark()
   })
 end
 
+--- Return the color table for the black-white light variant.
+---@return table Color table for the black-white light variant.
 local function black_white_light()
   local base6   = "#484840"
   local red     = "#B82020"
@@ -269,6 +280,8 @@ end
 
 -- Blue Variant ----------------------------------------------------------------------------
 
+--- Return the color table for the blue light variant.
+---@return table Color table for the blue light variant.
 local function blue_light()
   local base6   = "#384858"
   local red     = "#C02818"
@@ -316,6 +329,8 @@ local function blue_light()
   })
 end
 
+--- Return the color table for the blue dark variant.
+---@return table Color table for the blue dark variant.
 local function blue_dark()
   local base6   = "#5A7A98"
   local red     = "#F06040"
@@ -365,6 +380,8 @@ end
 
 -- Green Variant ---------------------------------------------------------------------------
 
+--- Return the color table for the green light variant.
+---@return table Color table for the green light variant.
 local function green_light()
   local base6   = "#3A5C3A"
   local red     = "#C03830"
@@ -412,6 +429,8 @@ local function green_light()
   })
 end
 
+--- Return the color table for the green dark variant.
+---@return table Color table for the green dark variant.
 local function green_dark()
   local base6   = "#6E8A70"
   local red     = "#E84040"
@@ -461,6 +480,8 @@ end
 
 -- Gray Variant  ---------------------------------------------------------------------------
 
+--- Return the color table for the gray dark variant.
+---@return table Color table for the gray dark variant.
 local function gray_dark()
   local base6   = "#686868"
   local red     = "#E84040"
@@ -508,6 +529,8 @@ local function gray_dark()
   })
 end
 
+--- Return the color table for the gray light variant.
+---@return table Color table for the gray light variant.
 local function gray_light()
   local base6   = "#606060"
   local red     = "#B82020"
@@ -555,6 +578,110 @@ local function gray_light()
   })
 end
 
+-- Rougier's Variant -----------------------------------------------------------------------
+
+--- Return the color table for th Rougier's light variant, the original variant of NANO
+--- theme.
+---@return table Color table for the rougier's light variant.
+local function rougier_light()
+  local base6   = "#546E7A"  -- Blue Grey / L600
+  local red     = "#B71C1C"  -- Material Red / L900
+  local green   = "#2E7D32"  -- Material Green / L800
+  local yellow  = "#F9A825"  -- Material Amber / L800
+  local blue    = "#1565C0"  -- Material Blue / L800
+  local magenta = "#6A1B9A"  -- Material Purple / L800
+  local cyan    = "#00838F"  -- Material Cyan / L800
+  local nano_foreground_color = "#37474F"  -- Blue Grey / L800
+
+  return build_colors({
+    base0 = "#FFFFFF",  -- White
+    base1 = "#FAFAFA",  -- Very Light Grey
+    base2 = "#ECEFF1",  -- Blue Grey / L50
+    base3 = "#CFD8DC",  -- Blue Grey / L100
+    base4 = "#B0BEC5",  -- Blue Grey / L200
+    base5 = "#90A4AE",  -- Blue Grey / L300
+    base6 = base6,
+    base7 = "#263238",  -- Blue Grey / L900
+
+    nano_foreground_color = nano_foreground_color,
+    nano_background_color = "#FFFFFF",
+    nano_highlight_color  = "#FAFAFA",
+    nano_subtle_color     = "#ECEFF1",
+    nano_veryfaded_color  = "#CFD8DC",
+    nano_faded_color      = "#90A4AE",  -- Blue Grey / L300
+    nano_salient_color    = "#673AB7",  -- Deep Purple / L500
+    nano_strong_color     = "#263238",  -- Blue Grey / L900
+    nano_popout_color     = "#FFAB91",  -- Deep Orange / L200
+    nano_critical_color   = "#FF6F00",  -- Amber / L900
+
+    terminal_color_0  = base6,
+    terminal_color_1  = red,
+    terminal_color_2  = green,
+    terminal_color_3  = yellow,
+    terminal_color_4  = blue,
+    terminal_color_5  = magenta,
+    terminal_color_6  = cyan,
+    terminal_color_8  = base6,
+    terminal_color_9  = red,
+    terminal_color_10 = green,
+    terminal_color_11 = yellow,
+    terminal_color_12 = blue,
+    terminal_color_13 = magenta,
+    terminal_color_14 = cyan,
+  })
+end
+
+--- Return the color table for th Rougier's dark variant, the original variant of NANO
+--- theme.
+---@return table Color table for the rougier's dark variant.
+local function rougier_dark()
+  local base6   = "#7B88A1"  -- Nord between Polar Night and Snow Storm
+  local red     = "#BF616A"  -- Nord Aurora / nord11
+  local green   = "#A3BE8C"  -- Nord Aurora / nord14
+  local yellow  = "#EBCB8B"  -- Nord Aurora / nord13
+  local blue    = "#81A1C1"  -- Nord Frost  / nord9
+  local magenta = "#B48EAD"  -- Nord Aurora / nord15
+  local cyan    = "#88C0D0"  -- Nord Frost  / nord8
+  local nano_foreground_color = "#ECEFF4"  -- Nord Snow Storm 3 / nord6
+
+  return build_colors({
+    base0 = "#2E3440",  -- Nord Polar Night 0 / nord0
+    base1 = "#3B4252",  -- Nord Polar Night 1 / nord1
+    base2 = "#434C5E",  -- Nord Polar Night 2 / nord2
+    base3 = "#4C566A",  -- Nord Polar Night 3 / nord3
+    base4 = "#D8DEE9",  -- Nord Snow Storm 1  / nord4
+    base5 = "#E5E9F0",  -- Nord Snow Storm 2  / nord5
+    base6 = base6,
+    base7 = "#ECEFF4",  -- Nord Snow Storm 3  / nord6
+
+    nano_foreground_color = nano_foreground_color,
+    nano_background_color = "#2E3440",  -- nord0
+    nano_highlight_color  = "#3B4252",  -- nord1
+    nano_subtle_color     = "#434C5E",  -- nord2
+    nano_veryfaded_color  = "#4C566A",  -- nord3
+    nano_faded_color      = "#677691",
+    nano_salient_color    = "#81A1C1",  -- Nord Frost / nord9
+    nano_strong_color     = "#ECEFF4",  -- nord6
+    nano_popout_color     = "#D08770",  -- Nord Aurora / nord12
+    nano_critical_color   = "#EBCB8B",  -- Nord Aurora / nord13
+
+    terminal_color_0  = base6,
+    terminal_color_1  = red,
+    terminal_color_2  = green,
+    terminal_color_3  = yellow,
+    terminal_color_4  = blue,
+    terminal_color_5  = magenta,
+    terminal_color_6  = cyan,
+    terminal_color_8  = base6,
+    terminal_color_9  = red,
+    terminal_color_10 = green,
+    terminal_color_11 = yellow,
+    terminal_color_12 = blue,
+    terminal_color_13 = magenta,
+    terminal_color_14 = cyan,
+  })
+end
+
 --------------------------------------------------------------------------------------------
 --                                    Public Functions                                    --
 --------------------------------------------------------------------------------------------
@@ -566,6 +693,7 @@ local light_variants = {
   blue        = blue_light,
   green       = green_light,
   gray        = gray_light,
+  rougier     = rougier_light,
 }
 
 local dark_variants = {
@@ -574,8 +702,11 @@ local dark_variants = {
   blue        = blue_dark,
   green       = green_dark,
   gray        = gray_dark,
+  rougier     = rougier_dark,
 }
 
+--- Return the color table for the currently active background and variant.
+---@return table Color table for the active theme variant.
 function M.get()
   local opts     = require("nano-theme").options
   local is_light = vim.o.background == "light"
